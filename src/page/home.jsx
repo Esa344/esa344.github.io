7ba2c2ba59d2f38b244bd11bf578a55f10bc2f5f
esa344 <hackblock12@gmail.com>
2024-09-04 21:49:09 +0700

    update

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {useState} from 'react';
 import stylink from 'function';
 
 const Home = () => {
-  return(
+  const [sideOpen, setSideOpen] = useState(false);
+  const [topLogoOpacity, setTopLogoOpacity] = useState(true);
+
+  function wSidebarOpen() {
+    const newMarginLeft = sideOpen ? '0vw' : '-60vw';
+    const newOpacity = topLogoOpacity ? '100%' : '0%';
+    setSideOpen(!sideOpen);
+    setTopLogoOpacity(!topLogoOpacity);
+
+    setTimeout(() => {
+      document.querySelector('.aside').style.marginLeft = newMarginLeft;
+      document.querySelector('.topLogo').style.opacity = newOpacity;
+    }, 0);
+  }
+
+  return (
     <div className='homeCanvas'>
       <div className='topBar'>
-            <aside className='aside'>
-              <div className='closeSide'>
-                <div className='xOne'/>
-                <div className='xTwo'/>
-              </div>
-            </aside>
-        <img src='/src/private/aSideAction.png' className='aSideAction'/>
-        <img src='/src/private/horizontalLogo.png' className='topLogo'/>
+        <aside className='aside'>
+          <div
+          className='closeSide'
+          onClick={wSidebarOpen}
+          >
+            <div className='xOne'/>
+            <div className='xTwo'/>
+          </div>
+        </aside>
+        <img 
+          src='/src/private/aSideAction.png' 
+          className='aSideAction' 
+          onClick={wSidebarOpen}
+          alt='Toggle Sidebar'
+        />
+        <img
+        src='/src/private/horizontalLogo.png'
+        className='topLogo'
+        alt='Logo'/>
       </div>
     </div>
-    )
+  );
 }
 
 stylink('src/page/home.css', 'mobile')
 
-export default Home
+export default Home;
