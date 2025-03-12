1045f2e0c755d0e794464c14185f02c0c2d6ccd7
esa344 <hackblock12@gmail.com>
2025-03-12 19:23:46 +0700

    update

--- assets/data.jsx
+++ assets/data.jsx
@@ -7,6 +7,14 @@ const Data = [
     },
     desc: 'poster/brosur makanan untuk anak sekolah, 2 buah makanan dengan penjelas harga, warna merah cerah di gradasi dengan merah hati dengan aksen oranye '
   },
+  {
+    name: 'poster web binar/seminar',
+    url: {
+      canva: 'https://www.canva.com/design/DAGhfsd1_Qs/mP6COcBKhxYg_vy3SzhrWg/view?utm_content=DAGhfsd1_Qs&utm_campaign=share_your_design&utm_medium=link2&utm_source=shareyourdesignpanel',
+      image: 'Porto2.png'
+    },
+    desc: 'poster/brosur seminar dengan warna colat cream'
+  }
   
 ]
 
